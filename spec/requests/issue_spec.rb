9d8c53a87bcf9ed96781cf5ccb7126f03827a90e
vnikiporov <vnikiporov@zynga.com>
2022-02-28 01:57:01 -0500

    [*] prettify_specs

--- spec/requests/issue_spec.rb
+++ spec/requests/issue_spec.rb
@@ -8,6 +8,11 @@ describe IssuesController, :type => :controller do
     let(:issue1) { Issue.new(title: "AAA Title", issue_priority_id: priority.id, issue_status_id: status.id, discipline_id: discipline.id) }
     let(:issue2) { Issue.new(title: "BBB Title", issue_priority_id: priority.id, issue_status_id: status.id, discipline_id: discipline.id) }
 
+    before do
+      issue1.save
+      issue2.save
+    end
+
     it 'should fetch index' do
       get :index, :format => 'json'
       expect(response.content_type).to eq "application/json"
@@ -15,10 +20,8 @@ describe IssuesController, :type => :controller do
       expect(json_response.map { |issue| issue.fetch('title') }.uniq.count).to eq Issue.all.count
     end
 
-    it 'should return records selectively based on search params (with discipline)' do
-      issue1.save
+    it 'should return records selectively, based on proper search params' do
       issue2.assignees << User.last
-      issue2.save
 
       get :index, format: 'json', params: {
         assignee: { 'id' => User.last.id.inspect },
@@ -30,28 +33,28 @@ describe IssuesController, :type => :controller do
       expect(json_response.first['title']).to eq issue2.title
     end
 
-    it 'should return records selectively based on search params (user is unassigned)' do
-      issue1.save
-      issue2.save
-
+    it 'should not return records, based on faulty search params: unassigned user' do
       get :index, format: 'json', params: {
         assignee: { 'id' => User.last.id.inspect },
         status:   { id: status.id },
         priority: { value: priority.value, type: 'exact' }
       }
-      expect(json_response.count).to eq 0
+      expect(json_response).to eq []
     end
 
-    it 'should return records selectively based on search params (only on priority)' do
-      issue1.save
-      issue2.save
-
-      # default behaviour: requested priority  and below (down to highest importance)
+    # default behaviour:
+    it 'should return records selectively, based on search params: priority down to highest importance' do
       get :index, format: 'json', params: { priority: { value: 1 } }
-
       expect(json_response).not_to be_empty
       priority_ids = json_response.map {|i| i['issue_priority_id'] }.uniq
       expect(IssuePriority.find(priority_ids).map(&:label)).to eq IssuePriority.where(value: (0..1).to_a).map &:label
+    end
+
+    it 'should return records selectively, based on search params: exact priority' do
+      get :index, format: 'json', params: { priority: { value: 1, type: 'exact' } }
+      expect(json_response.count).to be > 1
+      priority_ids = json_response.map {|i| i['issue_priority_id'] }.uniq
+      expect(priority_ids.count).to eq 1
     end
   end
 end
