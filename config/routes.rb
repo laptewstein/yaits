81c2f945d2c7566aa005d45be92be6c35548f59e
vnikiporov <vnikiporov@zynga.com>
2022-02-28 01:07:24 -0500

    [*] recursive filtering over params

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,3 @@
 Rails.application.routes.draw do
   resources :issues
 end
-
-
-# require 'rails/generators/rails/scaffold_controller/scaffold_controller_generator'
-#
-# patcher = Module.new do
-#   extend ActiveSupport::Concern
-#
-#   included do
-#     hook_for :resource_route, required: true
-#   end
-# end
-#
-# Rails::Generators::ScaffoldControllerGenerator.send :include, patcher
