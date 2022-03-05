a9c7b989df363532103d7a3da1be01edb6023dc5
vnikiporov <vnikiporov@zynga.com>
2022-03-05 01:46:22 -0500

    [bootstrapping] sample seeding values for `rake db:seed`

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,38 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+disciplines = [
+  "Engineering",
+  "UX",
+  "Analytics",
+  "CS",
+  "Marketing",
+  "Finance",
+  "Sales",
+  "Investor Relations",
+  "Strategic Partnerships"
+]
+statuses    = [
+  "Open",
+  "In progress",
+  "In review",
+  "Closed",
+  "Reopened",
+  "Wontfix"
+]
+priorities  = [
+  ["P0", 0],
+  ["P1", 1],
+  ['High', 2],
+  ['P2', 3],
+  ['Medium', 4],
+  ['Low', 5]
+]
+users       = [
+  { name: 'CSAdmin',  email: 'csadmin@downtowncore.ca' },
+  { name: 'Oncall',   email: 'oncall@bigtech.ca'},
+  { name: 'WatchBot', email: 'watchbot@downtowncore.ca' },
+  { name: 'IT-Desk', email: 'desk@tinytech.com'}
+]
+
+priorities.each { |k, v| IssuePriority.create(label: k, value: v) }
+disciplines.each { |e| Discipline.create(label: e) }
+statuses.each { |e| IssueStatus.crete(label: e) }
+User.create(users)
